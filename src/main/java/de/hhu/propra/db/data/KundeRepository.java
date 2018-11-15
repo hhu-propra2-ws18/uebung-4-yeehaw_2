c9c5459c2b29a20ac602e5f17cdf2b616e259daa
Aljosha <almei105@hhu.de>
2018-11-15 15:20:47 +0100

    Add List<Kunde> findAll in Repository and Controller

--- src/main/java/de/hhu/propra/db/data/KundeRepository.java
+++ src/main/java/de/hhu/propra/db/data/KundeRepository.java
@@ -3,6 +3,8 @@ package de.hhu.propra.db.data;
 import de.hhu.propra.db.entities.Kunde;
 import org.springframework.data.repository.CrudRepository;
 
-public interface KundeRepository extends CrudRepository<Kunde, Long> {
+import java.util.List;
 
+public interface KundeRepository extends CrudRepository<Kunde, Long> {
+    List<Kunde> findAll();
 }
