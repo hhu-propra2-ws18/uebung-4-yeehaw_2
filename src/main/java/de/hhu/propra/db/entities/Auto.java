fdd0b9cea0671d167d90ef71258d69bb5592db02
zn5 <jakob.kalus@hhu.de>
2018-11-15 14:53:38 +0100

    Implement Auto.java

--- src/main/java/de/hhu/propra/db/entities/Auto.java
+++ src/main/java/de/hhu/propra/db/entities/Auto.java
@@ -1,5 +1,21 @@
 package de.hhu.propra.db.entities;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class Auto{
+
+	@Id
+	@GeneratedValue
+	private long id;
+
+	private String marke;
+	private String farbe;
+	private int hubraum;
 
 }
